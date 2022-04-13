1dea1fb5d39a64069bb11d89046b663ae4ccd7fc
Habibur96 <habiburrahmannayan66@gmail.com>
2022-04-13 19:57:06 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+document.getElementById('cl').addEventListener('click', function () {
+    const a = document.getElementById('value');
+    const b = a.value
+    const num = parseFloat(b);
+    // console.log('7')
+    a.value = ''
+
+    
+})
+
+
+// document.getElementById('deposit-button').addEventListener('click', function () {
+
+    // //get the amount deposited
+    // const a = document.getElementById('click-deposit')
+
+    // const c = a.value;
+    // const num2 = parseFloat(c);
+    // a.value = '';
+    // ======================================================
+    //update deposit total
+    // const b = document.getElementById('dolar');
+    // const e = b.innerText;
+    // const num1 = parseFloat(e);
